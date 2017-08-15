2c341b9362b6076fc5d4435aac2d11ec208ab93a
Michaël Vachette <mvachette@nuxeo.com>
2017-08-15 14:07:54 -0700

    Add Direct Download And Cache Params

--- nuxeo-s3-simple-blobprovider-core/src/main/java/org/nuxeo/labs/s3/simple/blobprovider/SimpleS3Blobprovider.java
+++ nuxeo-s3-simple-blobprovider-core/src/main/java/org/nuxeo/labs/s3/simple/blobprovider/SimpleS3Blobprovider.java
@@ -20,31 +20,43 @@
 package org.nuxeo.labs.s3.simple.blobprovider;
 
 import com.amazonaws.ClientConfiguration;
+import com.amazonaws.HttpMethod;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.regions.Region;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.AmazonS3Exception;
+import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.nuxeo.common.file.FileCache;
 import org.nuxeo.common.file.LRUFileCache;
+import org.nuxeo.common.utils.RFC2231;
+import org.nuxeo.common.utils.SizeUtils;
 import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.NuxeoException;
 import org.nuxeo.ecm.core.blob.AbstractBlobProvider;
 import org.nuxeo.ecm.core.blob.BlobManager;
 import org.nuxeo.ecm.core.blob.ManagedBlob;
 import org.nuxeo.ecm.core.blob.SimpleManagedBlob;
+import org.nuxeo.ecm.core.io.download.DownloadHelper;
 import org.nuxeo.ecm.core.model.Document;
 import org.nuxeo.runtime.api.Framework;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Date;
 import java.util.Map;
 
 public class SimpleS3Blobprovider extends AbstractBlobProvider {
@@ -59,7 +71,21 @@ public class SimpleS3Blobprovider extends AbstractBlobProvider {
 
     public static final String AWS_SECRET_PROPERTY = "awssecret";
 
+    public static final String DIRECTDOWNLOAD_PROPERTY = "directdownload";
+
+    public static final String DIRECTDOWNLOAD_EXPIRE_PROPERTY = "directdownload.expire";
+
+    public static final String CACHE_SIZE_PROPERTY = "cachesize";
+
+    public static final String CACHE_COUNT_PROPERTY = "cachecount";
+
+    public static final String CACHE_MIN_AGE_PROPERTY = "cacheminage";
+
     String bucketName;
+
+    boolean directDownload;
+
+    int directDownloadExpire;
 
     AmazonS3 amazonS3;
 
@@ -75,13 +101,25 @@ public class SimpleS3Blobprovider extends AbstractBlobProvider {
         String bucketRegion = properties.get(BUCKET_REGION_PROPERTY);
         String awsID = properties.get(AWS_ID_PROPERTY);
         String awsSecret = properties.get(AWS_SECRET_PROPERTY);
+
+        String cacheSizeStr = properties.getOrDefault(CACHE_SIZE_PROPERTY, "100 mb");
+        String cacheCountStr = properties.getOrDefault(CACHE_COUNT_PROPERTY, "10000");
+        String minAgeStr = properties.getOrDefault(CACHE_MIN_AGE_PROPERTY, "3600");
+
+        directDownload = Boolean.parseBoolean(properties.getOrDefault(DIRECTDOWNLOAD_PROPERTY, "false"));
+        directDownloadExpire = Integer.parseInt(properties.getOrDefault(DIRECTDOWNLOAD_EXPIRE_PROPERTY,"1"));
+
         amazonS3 = new AmazonS3Client(new BasicAWSCredentials(awsID,awsSecret),new ClientConfiguration());
         amazonS3.setRegion(Region.getRegion(Regions.fromName(bucketRegion)));
-        initializeCache(20000000000l,10,3600);
+        initializeCache(SizeUtils.parseSizeInBytes(cacheSizeStr),Long.parseLong(cacheCountStr),Long.parseLong(minAgeStr));
+
     }
 
     @Override
     public Blob readBlob(BlobManager.BlobInfo blobInfo) throws IOException {
+        if (blobInfo == null || blobInfo.key == null) {
+            throw new IOException("Invalid blobinfo: "+blobInfo);
+        }
         return new SimpleManagedBlob(blobInfo);
     }
 
@@ -111,10 +149,14 @@ public class SimpleS3Blobprovider extends AbstractBlobProvider {
 
     public File getFileFromCache(ManagedBlob blob) throws IOException {
         String key = blob.getKey().split(":")[1];
-        GetObjectRequest request = new GetObjectRequest(getBucketName(),key);
-        S3Object object = getClient().getObject(request);
+        S3Object object;
+        try {
+            GetObjectRequest request = new GetObjectRequest(getBucketName(),key);
+            object = getClient().getObject(request);
+        } catch (AmazonS3Exception e) {
+            throw new IOException(String.format("Could get key %s in bucket %s",key,bucketName));
+        }
         String etag = object.getObjectMetadata().getETag();
-
         File cachedFile = fileCache.getFile(etag);
         if (cachedFile == null) {
             File tmp = fileCache.getTempFile();
@@ -122,7 +164,67 @@ public class SimpleS3Blobprovider extends AbstractBlobProvider {
             fileCache.putFile(etag, tmp);
             cachedFile = fileCache.getFile(etag);
         }
+        object.close();
         return cachedFile;
+    }
+
+
+    protected boolean isDirectDownload() {
+        return directDownload;
+    }
+
+    @Override
+    public URI getURI(ManagedBlob blob, BlobManager.UsageHint hint, HttpServletRequest servletRequest)
+            throws IOException {
+        if (hint != BlobManager.UsageHint.DOWNLOAD || !isDirectDownload()) {
+            return null;
+        }
+        String digest = blob.getKey();
+        // strip prefix
+        int colon = digest.indexOf(':');
+        if (colon >= 0) {
+            digest = digest.substring(colon + 1);
+        }
+
+        return getRemoteUri(digest, blob, servletRequest);
+    }
+
+
+    protected URI getRemoteUri(String digest, ManagedBlob blob, HttpServletRequest servletRequest) throws IOException {
+        String key = digest;
+        Date expiration = new Date();
+        expiration.setTime(expiration.getTime() + directDownloadExpire * 1000);
+        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucketName, key, HttpMethod.GET);
+        request.addRequestParameter("response-content-type", getContentTypeHeader(blob));
+        request.addRequestParameter("response-content-disposition", getContentDispositionHeader(blob, null));
+        request.setExpiration(expiration);
+        URL url = amazonS3.generatePresignedUrl(request);
+        try {
+            return url.toURI();
+        } catch (URISyntaxException e) {
+            throw new IOException(e);
+        }
+    }
+
+    protected String getContentTypeHeader(Blob blob) {
+        String contentType = blob.getMimeType();
+        String encoding = blob.getEncoding();
+        if (contentType != null && !StringUtils.isBlank(encoding)) {
+            int i = contentType.indexOf(';');
+            if (i >= 0) {
+                contentType = contentType.substring(0, i);
+            }
+            contentType += "; charset=" + encoding;
+        }
+        return contentType;
+    }
+
+    protected String getContentDispositionHeader(Blob blob, HttpServletRequest servletRequest) {
+        if (servletRequest == null) {
+            return RFC2231.encodeContentDisposition(blob.getFilename(), false, null);
+        } else {
+            return DownloadHelper.getRFC2231ContentDisposition(servletRequest, blob.getFilename());
+        }
     }
 
     /**
--- nuxeo-s3-simple-blobprovider-core/src/test/java/org/nuxeo/labs/s3/simple/blobprovider/TestSimples3Blobprovider.java
+++ nuxeo-s3-simple-blobprovider-core/src/test/java/org/nuxeo/labs/s3/simple/blobprovider/TestSimples3Blobprovider.java
@@ -27,6 +27,7 @@ import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.blob.BlobManager;
 import org.nuxeo.ecm.core.blob.BlobProvider;
+import org.nuxeo.ecm.core.blob.ManagedBlob;
 import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
@@ -38,6 +39,7 @@ import org.nuxeo.runtime.test.runner.FeaturesRunner;
 import javax.inject.Inject;
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -67,10 +69,42 @@ public class TestSimples3Blobprovider {
         info.key = "s3simple:"+"CB_102_55A-3_55A-1.mov";
         info.filename = "CB_102_55A-3_55A-1.mov";
         Blob blob = s3provider.readBlob(info);
-
         File tmp = Framework.createTempFile("nxtmp-", "");
         FileUtils.copyInputStreamToFile(blob.getStream(),tmp);
 
         Assert.assertTrue(tmp.length()==4074170);
     }
+
+    @Test
+    public void testGetDirectDownload() throws IOException, OperationException {
+        BlobProvider s3provider = new SimpleS3Blobprovider();
+        Map<String,String> properties = new HashMap<>();
+        properties.put("region",System.getProperty("simple.s3.region"));
+        properties.put("bucket",System.getProperty("simple.s3.bucket"));
+        properties.put("awsid",System.getProperty("simple.s3.awsid"));
+        properties.put("awssecret",System.getProperty("simple.s3.awssecret"));
+        properties.put("directdownload","true");
+        s3provider.initialize("s3simple",properties);
+        blobManager.getBlobProviders().put("s3simple",s3provider);
+        BlobManager.BlobInfo info = new BlobManager.BlobInfo();
+        info.key = "s3simple:"+"CB_102_55A-3_55A-1.mov";
+        info.filename = "CB_102_55A-3_55A-1.mov";
+        ManagedBlob blob = (ManagedBlob) s3provider.readBlob(info);
+        URI downloadURI = s3provider.getURI(blob, BlobManager.UsageHint.DOWNLOAD,null);
+        Assert.assertNotNull(downloadURI);
+    }
+
+    @Test(expected = IOException.class)
+    public void testGetBlobNull() throws IOException, OperationException {
+        BlobProvider s3provider = new SimpleS3Blobprovider();
+        Map<String,String> properties = new HashMap<>();
+        properties.put("region",System.getProperty("simple.s3.region"));
+        properties.put("bucket",System.getProperty("simple.s3.bucket"));
+        properties.put("awsid",System.getProperty("simple.s3.awsid"));
+        properties.put("awssecret",System.getProperty("simple.s3.awssecret"));
+        s3provider.initialize("s3simple",properties);
+        blobManager.getBlobProviders().put("s3simple",s3provider);
+        Blob blob = s3provider.readBlob(null);
+    }
+
 }
